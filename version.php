8fac8c070ead53d3309b62cf4919eb2a1d00f5df
Mark Sharp <mark.sharp@solent.ac.uk>
2024-04-04 13:26:58 +0100

    Require sample plugin

--- version.php
+++ version.php
@@ -32,4 +32,5 @@ $plugin->release = '4.5-r1';
 $plugin->dependencies = [
     'local_solsits' => ANY_VERSION,
     'local_quercus_tasks' => ANY_VERSION,
+    'assignfeedback_sample' => ANY_VERSION,
 ];
